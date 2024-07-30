24b5dabd24646a08f451c851ab0ec1c9376cdc50
victoroab <96608980+victoroab@users.noreply.github.com>
2024-07-30 15:09:41 +0100

    feat: can load previously saved data

--- src/components/routes/loans/profit-loss/other-bank.tsx
+++ src/components/routes/loans/profit-loss/other-bank.tsx
@@ -8,6 +8,7 @@ import {
   TableCell,
   TableHeader,
 } from '@/components/ui/table'
+import { useGetOB, useSendOB } from '@/lib/api/profit-loss/functions'
 import { OtherBankData } from '@/lib/api/profit-loss/schema'
 import { useState } from 'react'
 
@@ -15,6 +16,13 @@ export const OtherBank = ({ loanId }: { loanId: string }) => {
   const [rows, setRows] = useState<OtherBankData[]>([
     { bankName: '', accName: '', accNo: '', bal: 0 },
   ])
+
+  const lastSaved = useGetOB(loanId)
+  const loadSaved = () => {
+    if (lastSaved.data) {
+      setRows(lastSaved.data)
+    }
+  }
 
   const updateCell = (
     idx: number,
@@ -37,9 +45,32 @@ export const OtherBank = ({ loanId }: { loanId: string }) => {
     setRows((prev) => prev.filter((rows, idx) => idx != prev.length - 1))
   }
 
+  const ob = useSendOB()
+  const submit = () => {
+    if (
+      rows[rows.length - 1].accName === '' ||
+      rows[rows.length - 1].accNo === '' ||
+      rows[rows.length - 1].bal === 0 ||
+      rows[rows.length - 1].bankName === ''
+    ) {
+      alert('complete rows before submitting')
+      return
+    }
+
+    ob.mutate({
+      data: rows,
+      loanId: loanId,
+    })
+  }
+
   return (
-    <div className="w-full space-y-6">
-      <Table>
+    <div className="w-full space-y-6 py-3">
+      {lastSaved.data && (
+        <Button onClick={() => loadSaved()} variant={'secondary'}>
+          Load Previously Saved
+        </Button>
+      )}
+      <Table className="border">
         <TableHeader className="bg-purple-200">
           <TableRow>
             <TableHead>S/N</TableHead>
@@ -57,18 +88,21 @@ export const OtherBank = ({ loanId }: { loanId: string }) => {
                 <Input
                   id={'bankName' + idx}
                   onChange={(e) => updateCell(idx, e.target.value, 'bankName')}
+                  value={rows[idx].bankName}
                 />
               </TableCell>
               <TableCell className="border">
                 <Input
                   id={'accName' + idx}
                   onChange={(e) => updateCell(idx, e.target.value, 'accName')}
+                  value={rows[idx].accName}
                 />
               </TableCell>
               <TableCell className="border">
                 <Input
                   id={'accNo' + idx}
                   onChange={(e) => updateCell(idx, e.target.value, 'accNo')}
+                  value={rows[idx].accNo}
                 />
               </TableCell>
               <TableCell className="border">
@@ -77,6 +111,7 @@ export const OtherBank = ({ loanId }: { loanId: string }) => {
                   onChange={(e) =>
                     updateCell(idx, parseFloat(e.target.value), 'bal')
                   }
+                  value={rows[idx].bal}
                 />
               </TableCell>
             </TableRow>
@@ -92,7 +127,9 @@ export const OtherBank = ({ loanId }: { loanId: string }) => {
             Delete Row
           </Button>
         </div>
-        <Button className="">Save</Button>
+        <Button className="" onClick={() => submit()}>
+          Save
+        </Button>
       </div>
     </div>
   )

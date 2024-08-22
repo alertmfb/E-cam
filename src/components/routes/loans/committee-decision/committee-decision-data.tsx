7e954a417582c7b6da2da1c77f84d5e4fb6b1fdc
victoroab <96608980+victoroab@users.noreply.github.com>
2024-08-22 10:40:47 +0100

    ui: new committee decision data

--- src/components/routes/loans/committee-decision/committee-decision-data.tsx
+++ src/components/routes/loans/committee-decision/committee-decision-data.tsx
@@ -5,9 +5,19 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableHeader,
+} from '@/components/ui/table'
+import { Input } from '@/components/ui/input'
 import { ChevronDown } from 'lucide-react'
 import { useState } from 'react'
 import { CommitteeDecisionTable } from './cd-table'
+import { useCommitteeDecision } from '@/lib/api/committee-decision/functions'
 
 export const CommitteeDecisionData = ({ loanId }: { loanId: string }) => {
   const [content, setContent] = useState(false)
@@ -29,11 +39,96 @@ export const CommitteeDecisionData = ({ loanId }: { loanId: string }) => {
         {content && (
           <CardContent className="transition ease-in-out fade-in-30 delay-150">
             <div className="w-full flex flex-col gap-3">
-              <CommitteeDecisionTable loanId={loanId} />
+              <DataTable loanId={loanId} />
             </div>
           </CardContent>
         )}
       </Card>
     </div>
+  )
+}
+
+const DataTable = ({ loanId }: { loanId: string }) => {
+  const { data: fetched } = useCommitteeDecision(loanId)
+  return (
+    <Table className="border">
+      <TableHeader className="font-black">
+        <TableRow className="bg-purple-100 font-black">
+          <TableHead className="border text-black">S/N</TableHead>
+          <TableHead className="border text-black">NAME</TableHead>
+          <TableHead className="border text-black">DESIGNATION</TableHead>
+          <TableHead className="border text-black">AMOUNT</TableHead>
+          <TableHead className="border text-black">DURATION</TableHead>
+          <TableHead className="border text-black">CCD%</TableHead>
+          <TableHead className="border text-black">UPFRONT FEE %</TableHead>
+          <TableHead className="border text-black">INTEREST RATE %</TableHead>
+          <TableHead className="border text-black">OTHER COMMENT</TableHead>
+          <TableHead className="border text-black">DATE</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {fetched &&
+          fetched.map((row, idx) => (
+            <TableRow key={idx}>
+              <TableCell>{idx + 1}</TableCell>
+              <TableCell>
+                <Input className="w-40" readOnly value={row.name} />
+              </TableCell>
+
+              <TableCell>
+                <Input className="w-40" readOnly value={row.designation} />
+              </TableCell>
+
+              <TableCell>
+                <Input
+                  className="w-28"
+                  type="number"
+                  readOnly
+                  value={row.amount === 0 ? '' : row.amount}
+                />
+              </TableCell>
+
+              <TableCell>
+                <Input className="w-40" readOnly value={row.duration} />
+              </TableCell>
+
+              <TableCell>
+                <Input
+                  type="number"
+                  className="w-20"
+                  readOnly
+                  value={row.ccd === 0 ? '' : row.ccd}
+                />
+              </TableCell>
+
+              <TableCell>
+                <Input
+                  type="number"
+                  className="w-20"
+                  readOnly
+                  value={row.uf === 0 ? '' : row.uf}
+                />
+              </TableCell>
+
+              <TableCell>
+                <Input
+                  type="number"
+                  className="w-20"
+                  readOnly
+                  value={row.ir === 0 ? '' : row.ir}
+                />
+              </TableCell>
+
+              <TableCell>
+                <Input className="w-80" readOnly value={row.comment} />
+              </TableCell>
+
+              <TableCell>
+                <Input className="w-36" readOnly value={row.date} />
+              </TableCell>
+            </TableRow>
+          ))}
+      </TableBody>
+    </Table>
   )
 }

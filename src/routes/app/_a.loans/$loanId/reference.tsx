e04d8352e837ae892aaba4f1a70fcbaa1ea11480
victoroab <96608980+victoroab@users.noreply.github.com>
2024-08-26 12:20:58 +0100

    ui: reduced textarea size on small screens

--- src/routes/app/_a.loans/$loanId/reference.tsx
+++ src/routes/app/_a.loans/$loanId/reference.tsx
@@ -24,7 +24,7 @@ function Reference() {
   }
 
   return (
-    <div className="container w-full flex flex-col gap-3">
+    <div className="container w-full flex flex-col gap-3 p-3">
       <div className="flex items-center gap-5 justify-between flex-1 flex-wrap">
         <div className="flex items-center gap-3">
           <ArrowLeft
